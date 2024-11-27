0e415d4e982fcce281a5386466d01104cdff84bc
斑鸫 <39019284+a514154639@users.noreply.github.com>
2024-11-27 09:18:19 +0800

    Create video_capture.h

---
+++ video_capture.h
@@ -0,0 +1,74 @@
+#pragma once
+#include <string>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+/*Include ffmpeg header file*/
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libswscale/swscale.h>
+#include <libavdevice/avdevice.h>
+
+#include <libavutil/imgutils.h>
+#include <libavutil/opt.h>
+#include <libavutil/mathematics.h>
+#include <libavutil/samplefmt.h>
+
+#ifdef __cplusplus
+}
+#endif
+#include "nvjmi.h"
+
+class VideoCapture
+{
+public:
+    explicit VideoCapture(const std::string &filename);
+    ~VideoCapture();
+    bool isOpened() { return _isOpened; };
+    VideoCapture &operator>>(cv::Mat &image);
+    VideoCapture(){};
+    bool open(const std::string &filename);
+    bool read(cv::Mat &image);
+    bool read_old(cv::Mat &image);
+    void release();
+    
+
+//private:
+    
+    
+
+private:
+    
+    mutable bool _isOpened{false};
+    AVCodec *_codec{NULL};
+    AVCodecContext *_ctx{NULL};
+    AVCodecParameters *_origin_par{NULL};
+    AVFrame *_fr{NULL};
+    AVFrame *_frBGR{NULL};
+    AVPacket *_pkt{NULL};
+    AVFormatContext *_fmt_ctx{NULL};
+    struct SwsContext *_img_convert_ctx{NULL};
+    uint8_t *_out_buffer{NULL};
+    uchar * out_data{NULL};
+    int _size{-1};
+    int _video_stream{-1};
+    jmi::nvJmiCtxParam jmi_ctx_param;
+    jmi::nvJmiCtx *jmi_ctx_{NULL};
+    jmi::nvPacket nvpacket;
+    jmi::nvFrameMeta nvframe_meta;
+
+};
+
+extern "C" void Init_uri(int i, const char * uri);
+extern "C" bool isConnect(int i);
+//extern "C" void Getbyte(unsigned char *image,int i);
+extern "C" void reConnect(int i);
+extern "C" int Getbyte( int i,int& width, int& height, int& size, unsigned char*& data);
+//extern "C" int Getbyte( int i, cv::Mat **returnframe);
+
+
