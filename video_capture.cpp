382bb60a7dc6c1ec25121f3feeb137f673fcd7d1
斑鸫 <39019284+a514154639@users.noreply.github.com>
2025-07-10 14:15:25 +0800

    Update video_capture.cpp
    支持mpp

--- video_capture.cpp
+++ video_capture.cpp
@@ -1,6 +1,7 @@
 #include "video_capture.h"
 #include <iostream>
 
+
 VideoCapture::VideoCapture(const std::string &filename)
 {
     _isOpened = open(filename);
@@ -13,157 +14,110 @@ VideoCapture::~VideoCapture()
 
 
 
-bool VideoCapture::open(const std::string &filename)
-{
+
+bool VideoCapture::open(const std::string &filename){
     AVDictionary *options = NULL;
-    AVStream *st = nullptr;
-    av_dict_set(&options, "buffer_size", "1024000", 0); //设置缓存大小,1080p可将值跳到最大
-    av_dict_set(&options, "rtsp_transport", "tcp", 0);  //以tcp的方式打开,
-    av_dict_set(&options, "stimeout", "5000000", 0);    //设置超时断开链接时间，单位us
-    av_dict_set(&options, "max_delay", "500000", 0);    //设置最大时延
 
-     // 分配并初始化一个AVFormatContext
-    _fmt_ctx = avformat_alloc_context();
-    if (!_fmt_ctx) {
-        av_log(NULL, AV_LOG_ERROR, "Failed to allocate AVFormatContext\n");
-        return false;
-    }
+    //    av_register_all();
+    printf("avformat_network_init start\n");
+    avformat_network_init();
+    // 设置读流相关的配置信息
+    av_dict_set(&options, "buffer_size", "1024000", 0); // 设置缓存大小
+    av_dict_set(&options, "rtsp_transport", "tcp", 0);  // 以udp的方式打开,
+    av_dict_set(&options, "stimeout", "5000000", 0);    // 设置超时断开链接时间，单位us
+    av_dict_set(&options, "max_delay", "500000", 0);    // 设置最大时延
 
-    // 设置非阻塞模式
-    _fmt_ctx->flags |= AVFMT_FLAG_NONBLOCK;
+    printf("avformat_alloc_context start\n");
 
-    int result = avformat_open_input(&_fmt_ctx, filename.c_str(), NULL, &options);
-    if (result < 0)
+    //_fmt_ctx = avformat_alloc_context(); // 用来申请AVFormatContext类型变量并初始化默认参数,申请的空间
+
+    printf("avformat_open_input start\n");
+
+    // 打开网络流或文件流
+    if (avformat_open_input(&_fmt_ctx, filename.c_str(), NULL, &options) != 0)
     {
-        av_log(NULL, AV_LOG_ERROR, "Can't open file\n");
-        avformat_free_context(_fmt_ctx);
-        _fmt_ctx = nullptr;
-        return false;
+        printf("Couldn't open input stream.\n");
+        return 0;
     }
 
-    result = avformat_find_stream_info(_fmt_ctx, NULL);
-    if (result < 0)
+    printf("avformat_find_stream_info start\n");
+
+    // 获取视频文件信息
+    if (avformat_find_stream_info(_fmt_ctx, NULL) < 0)
     {
-        av_log(NULL, AV_LOG_ERROR, "Can't get stream info\n");
-        avformat_close_input(&_fmt_ctx);
-        return false;
+        printf("Couldn't find stream information.\n");
+        return 0;
     }
 
-    for (int i = 0; i < _fmt_ctx->nb_streams; i++) {
-        av_dump_format(_fmt_ctx, i, filename.c_str(), 0);
-    }
-    int height = 0;
-    int width = 0;
+    printf("avformat_find_stream_info end\n");
 
-    bool has_264_video = false;
-    for (int i = 0; i < _fmt_ctx->nb_streams; i++) {
-        st = _fmt_ctx->streams[i];
-        switch (st->codecpar->codec_type) {
-            case AVMEDIA_TYPE_VIDEO:
-                height = st->codecpar->height;
-                width = st->codecpar->width;
-                has_264_video = true;
-                break;
-            default:
-                break;
+    // 查找码流中是否有视频流
+    unsigned i = 0;
+    videoindex = av_find_best_stream(_fmt_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0);
+    for (i = 0; i < _fmt_ctx->nb_streams; i++)
+        if (_fmt_ctx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
+        {
+            videoindex = i;
+            break;
         }
+    if (videoindex == -1)
+    {
+        printf("Didn't find a video stream.\n");
+        return 0;
     }
+    
+    // 获取图像宽高
+    int srcWidth = _fmt_ctx->streams[videoindex]->codecpar->width;
+    int srcHeight = _fmt_ctx->streams[videoindex]->codecpar->height;
 
-    //初始化AVPacket
+    printf("srcWidth is %d, srcHeight is %d\n", srcWidth, srcHeight);
+
+    // _pkt = (AVPacket *)av_malloc(sizeof(AVPacket)); // 申请空间，存放的每一帧数据 （h264、h265）
+    // av_new_packet(_pkt, srcWidth * srcHeight);
+
     _pkt = av_packet_alloc();
     if (!_pkt)
     {
         av_log(NULL, AV_LOG_ERROR, "Cannot allocate packet\n");
         return false;
     }
-    
-    jmi_ctx_param.resize_width = width;
-    jmi_ctx_param.resize_height = height;
-
-    _size = avpicture_get_size(AV_PIX_FMT_BGR24, width, height);
-    _out_buffer = (uint8_t *)av_malloc(_size);
-
-    jmi_ctx_param.coding_type = jmi::NV_VIDEO_CodingH264;
-    //jmi_ctx_param.coding_type = jmi::NV_VIDEO_CodingHEVC;
-    std::string dec_name = "h264_dec_";
-    if (filename.size() >= 5) {
-        char fifth_last_char = filename[filename.size() - 5];
-        dec_name += fifth_last_char;
-    } else {
-        // filename长度不足5个字符，可以选择添加默认字符或处理错误
-        dec_name += '_'; // 例如使用下划线作为默认字符
+    _fr = av_frame_alloc();
+    if (!_fr)
+    {
+        av_log(NULL, AV_LOG_ERROR, "Can't allocate frame\n");
+        return false;
     }
-    std::cout << dec_name << std::endl;
-    jmi_ctx_ = jmi::nvjmi_create_decoder(dec_name.data(), &jmi_ctx_param);
-    
 
+    _frBGR = av_frame_alloc();
+    if (!_frBGR)
+    {
+        av_log(NULL, AV_LOG_ERROR, "Can't allocate frame\n");
+        return false;
+    }
+    
+    decoder = avcodec_find_decoder_by_name("h264_rkmpp");
+    _ctx = avcodec_alloc_context3(decoder);
+    avcodec_parameters_to_context(_ctx, _fmt_ctx->streams[videoindex]->codecpar);
+    if (avcodec_open2(_ctx, decoder, nullptr) < 0) {
+        std::cerr << "Could not open decoder." << std::endl;
+        return -1;
+    }
+
+    _size = av_image_get_buffer_size(AV_PIX_FMT_BGR24, _ctx->width, _ctx->height,1);
+    _out_buffer = (uint8_t *)av_malloc(_size);
+    av_image_fill_arrays(_frBGR->data, _frBGR->linesize, _out_buffer, 
+                         AV_PIX_FMT_BGR24, _ctx->width, _ctx->height, 1);
+    _img_convert_ctx = sws_getContext(_ctx->width, _ctx->height, _ctx->pix_fmt, _ctx->width, _ctx->height, AV_PIX_FMT_BGR24, SWS_BICUBIC, NULL, NULL, NULL);
+
+    printf("decode init finish.\n");
+    
     _isOpened = true;
     return true;
+
 }
-
-
 
 
 bool VideoCapture::read(cv::Mat &image)
-{
-    if (!_isOpened)
-    {
-        av_log(NULL, AV_LOG_ERROR, "Cannot open file\n");
-        return false;
-    }
-    int ret = 0;
-    while (ret >= 0) {
-        ret = av_read_frame(_fmt_ctx, _pkt);
-        if (ret != 0) {
-            std::cout << "av_read_frame fail " << ret << std::endl;
-            av_packet_unref(_pkt);
-            _isOpened = false;
-            //break;
-            return false;
-        } else {
-            auto payload = _pkt->data;
-            auto payload_size = _pkt->size;
-            nvpacket.payload_size = payload_size;
-            nvpacket.payload = payload;
-            
-            if (payload != nullptr) {
-                ret = jmi::nvjmi_decoder_put_packet(jmi_ctx_, &nvpacket);
-                //std::cout << "nvjmi_decoder_put_packet " << ret << std::endl;
-                if(ret == jmi::NVJMI_ERROR_STOP) {
-                    av_log(NULL, AV_LOG_ERROR, "frameCallback: nvjmi decode error, frame callback EOF!");
-                    return false;
-                }
-                while (ret >= 0) {
-                    //jmi::nvFrameMeta nvframe_meta;
-                    ret = jmi::nvjmi_decoder_get_frame_meta(jmi_ctx_, &nvframe_meta);
-                    //std::cout << "nvjmi_decoder_get_frame_meta " << ret << std::endl;
-                    if (ret < 0)
-                    {
-                        av_packet_unref(_pkt);
-                        return false;
-                        //break;
-                    }
-
-                    ret = jmi::nvjmi_decoder_retrieve_frame_data(jmi_ctx_, &nvframe_meta, (void *)_out_buffer); 
-                    //std::cout << "nvjmi_decoder_retrieve_frame_data " << ret << std::endl;
-                    //cv::Mat(nvframe_meta.height, nvframe_meta.width, CV_8UC3, _out_buffer).copyTo(image);   
-                    image = cv::Mat(nvframe_meta.height, nvframe_meta.width, CV_8UC3, _out_buffer);					
-                    av_packet_unref(_pkt);		
-                    return true;
-                    
-                }
-            }
-        }       
-    }
-    
-    //av_packet_unref(_pkt);	
-    return false;
-}
-
-
-
-
-bool VideoCapture::read_old(cv::Mat &image)
 {
     if (!_isOpened)
     {
@@ -176,8 +130,8 @@ bool VideoCapture::read_old(cv::Mat &image)
     while (result >= 0)
     {
         result = av_read_frame(_fmt_ctx, _pkt);
-
-        if (result >= 0 && _pkt->stream_index != _video_stream)
+        //std::cout << "stream_index: " <<  _pkt->stream_index << " videoindex " << videoindex << std::endl;
+        if (result >= 0 && _pkt->stream_index != videoindex)
         {
             av_packet_unref(_pkt);
             continue;
@@ -190,7 +144,6 @@ bool VideoCapture::read_old(cv::Mat &image)
             if (_pkt->pts == AV_NOPTS_VALUE)
                 _pkt->pts = _pkt->dts = i;
             result = avcodec_send_packet(_ctx, _pkt);
-
         }
         av_packet_unref(_pkt);
 
@@ -199,10 +152,12 @@ bool VideoCapture::read_old(cv::Mat &image)
             av_log(NULL, AV_LOG_ERROR, "Error submitting a packet for decoding\n");
             return false;
         }
-
+         //std::cout << "result " << result  << std::endl;
         while (result >= 0)
         {
+            //std::cout << "start receive frame " << std::endl;
             result = avcodec_receive_frame(_ctx, _fr);
+            //std::cout << "finish receive frame " << std::endl;
             if (result == AVERROR_EOF)
             {
                 av_log(NULL, AV_LOG_ERROR, "End of file\n");
@@ -218,15 +173,8 @@ bool VideoCapture::read_old(cv::Mat &image)
                 av_log(NULL, AV_LOG_ERROR, "Error decoding frame\n");
                 return false;
             }
-
-            std::chrono::system_clock::time_point start = std::chrono::system_clock::now();    
-            //std::cout << "frame type: " << _fr->format << std::endl;
+            //std::cout << "start convert frame " << std::endl;
             sws_scale(_img_convert_ctx, (const uint8_t *const *)_fr->data, _fr->linesize, 0, _ctx->height, _frBGR->data, _frBGR->linesize); //YUV to RGB
-            //AVFrame2Img_gpu(_fr, _out_buffer);
-            auto now = std::chrono::system_clock::now();
-            std::chrono::duration<double,  std::milli> elapsed = now - start;
-            std::cout << "convert time: " << elapsed.count() << std::endl;
-
             cv::Mat(_ctx->height, _ctx->width, CV_8UC3, _out_buffer).copyTo(image);
             av_frame_unref(_fr);
             return true;
@@ -235,6 +183,8 @@ bool VideoCapture::read_old(cv::Mat &image)
     }
     return true;
 }
+
+
 
 VideoCapture &VideoCapture::operator>>(cv::Mat &image)
 {
@@ -251,8 +201,6 @@ void VideoCapture::release()
     avformat_close_input(&_fmt_ctx);
     avcodec_free_context(&_ctx);
     av_freep(&_out_buffer);
-    nvjmi_decoder_close(jmi_ctx_);
-    nvjmi_decoder_free_context(&jmi_ctx_);
 }
 
 
@@ -334,4 +282,29 @@ int Getbyte( int i,int& width, int& height, int& size, unsigned char*& data) {
     return 1;
 }
 
+int Getbyte_( int i,int& width, int& height, int& size, cv::Mat** returnframe) {
+    if (!videos[i].isOpened()) {
+        std::cout << "cam " << i << " link fail" << std::endl;
+        return 0;
+    }
 
+    cv::Mat frame;
+    //std::cout << "start read" << std::endl;  
+	videos[i].read(frame);
+    if( frame.empty()){
+        std::cout << "Input frame is empty" << std::endl;  
+            
+        videos[i].release();
+        return 0;  
+    }
+    width = frame.cols;
+    height = frame.rows;
+    size = 0;
+   
+     *returnframe = new cv::Mat(frame);  // 分配新内存并复制数据
+    delete *returnframe;  // It's safe to delete nullptr so no need to check
+    //*returnframe = new cv::Mat(frame); // Clone the frame to ensure it's persistent
+    //std::cout << &returnframe << std::endl;
+    //frame.release();
+    return 1;
+}
