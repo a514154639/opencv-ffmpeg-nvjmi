4cedc8b8cc9cc1b197c64b20c7dd2b9c6e83ea42
斑鸫 <39019284+a514154639@users.noreply.github.com>
2025-07-11 09:05:44 +0800

    Update video_capture.h

--- video_capture.h
+++ video_capture.h
@@ -3,9 +3,8 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <unistd.h>
-#include <map>
-//#include "media/mpi_dec.h"
+#include <memory>
+#include <queue>
 
 #ifdef __cplusplus
 extern "C"
@@ -15,22 +14,17 @@ extern "C"
 #include <libavformat/avformat.h>
 #include <libavcodec/avcodec.h>
 #include <libswscale/swscale.h>
-#include <libavdevice/avdevice.h>
-#include <libavfilter/avfilter.h>
-#include <libavfilter/buffersink.h>
-#include <libavfilter/buffersrc.h>
+//#include <libavdevice/avdevice.h>
+
 #include <libavutil/imgutils.h>
 #include <libavutil/opt.h>
 #include <libavutil/mathematics.h>
 #include <libavutil/samplefmt.h>
-#include <libavutil/hwcontext.h>
-#include <libavutil/pixfmt.h>
 
 #ifdef __cplusplus
 }
 #endif
-
-
+#include "nvjmi.h"
 
 class VideoCapture
 {
@@ -40,10 +34,14 @@ public:
     bool isOpened() { return _isOpened; };
     VideoCapture &operator>>(cv::Mat &image);
     VideoCapture(){};
-    //bool open(const std::string &filename);
     bool open(const std::string &filename);
     bool read(cv::Mat &image);
+    bool read_old(cv::Mat &image);
+    bool read_bac(cv::Mat &image);
     void release();
+    void reconnect(int i); 
+    int qframe_size{-1};
+    
     
 
 //private:
@@ -53,27 +51,35 @@ public:
 private:
     
     mutable bool _isOpened{false};
-    const AVCodec *decoder{NULL};
+    AVCodec *_codec{NULL};
     AVCodecContext *_ctx{NULL};
     AVCodecParameters *_origin_par{NULL};
     AVFrame *_fr{NULL};
     AVFrame *_frBGR{NULL};
     AVPacket *_pkt{NULL};
     AVFormatContext *_fmt_ctx{NULL};
+    AVRational time_base = {1, AV_TIME_BASE}; 
+    std::chrono::system_clock::time_point starttime{}; 
     struct SwsContext *_img_convert_ctx{NULL};
     uint8_t *_out_buffer{NULL};
     uchar * out_data{NULL};
     int _size{-1};
-    int videoindex{-1};
+    int _video_stream{-1};
+    jmi::nvJmiCtxParam jmi_ctx_param;
+    jmi::nvJmiCtx *jmi_ctx_{NULL};
+    jmi::nvPacket nvpacket;
+    jmi::nvFrameMeta nvframe_meta;
+    std::queue<cv::Mat> frame_queue;
+    //string _filename;
+    
 
 };
-
+std::string getcurrent_time();
 extern "C" void Init_uri(int i, const char * uri);
 extern "C" bool isConnect(int i);
 //extern "C" void Getbyte(unsigned char *image,int i);
 extern "C" void reConnect(int i);
 extern "C" int Getbyte( int i,int& width, int& height, int& size, unsigned char*& data);
-extern "C" int Getbyte_( int i,int& width, int& height, int& size, cv::Mat** returnframe);
 //extern "C" int Getbyte( int i, cv::Mat **returnframe);
 
 
